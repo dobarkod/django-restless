5ee5bedb849db23085577ddd34a854ac4a481ad5
Senko Rasic <senko.rasic@dobarkod.hr>
2012-08-09 12:17:11 +0200

    setup.py: PEP8 fixes

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'DjangoRestless',
-    version = '0.0.1',
-    author = 'Senko Rasic',
-    author_email = 'senko.rasic@goodcode.io',
-    description = 'A RESTful framework for Django',
-    license = 'MIT',
-    url = 'http://goodcode.io/',
-    classifiers = [
+    name='DjangoRestless',
+    version='0.0.1',
+    author='Senko Rasic',
+    author_email='senko.rasic@goodcode.io',
+    description='A RESTful framework for Django',
+    license='MIT',
+    url='http://goodcode.io/',
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Django",
@@ -20,7 +20,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 
-    packages = find_packages(),
+    packages=find_packages(),
 
-    install_requires = []
+    install_requires=[]
 )
