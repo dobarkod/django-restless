cb08db6829049038b044e12dbe3064c38d10f242
Senko Rasic <senko.rasic@dobarkod.hr>
2012-11-06 14:02:29 +0100

    bumping version to 0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DjangoRestless',
-    version='0.0.2',
+    version='0.0.3',
     author='Senko Rasic',
     author_email='senko.rasic@goodcode.io',
     description='A RESTful framework for Django',
